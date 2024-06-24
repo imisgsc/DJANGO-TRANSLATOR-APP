19ccbfc4f0bc73e953160d39e5ac791ff4c8e5c8
imisgsc <simigracia@gmail.com>
2024-06-24 22:23:56 +0530

    updated app

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ def index():
     translated_text = ''
     if request.method == 'POST':
         input_text = request.form['input_text']
-        translated_text = translator.translate(input_text, dest='en').text
+        source_lang = request.form['source_lang']
+        target_lang = request.form['target_lang']
+        translated_text = translator.translate(input_text, src=source_lang, dest=target_lang).text
     return render_template('index.html', translated_text=translated_text)
 
 if __name__ == '__main__':
