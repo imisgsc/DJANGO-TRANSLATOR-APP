cd6532039346640c7bdaed9d913a6b983e1648df
imisgsc <simigracia@gmail.com>
2024-06-24 22:18:46 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request
+from googletrans import Translator
 
 app = Flask(__name__)
+translator = Translator()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    translated_text = ''
+    if request.method == 'POST':
+        input_text = request.form['input_text']
+        translated_text = translator.translate(input_text, dest='en').text
+    return render_template('index.html', translated_text=translated_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
